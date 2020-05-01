3ed1b2af89553da943ddfde55fd256ebc02d3de1
杨波 <18310915680@163.com>
2020-05-02 01:35:17 +0800

    一个简易的cli工具

---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+const program = require('commander')
+const download = require('download-git-repo')
+const ora = require('ora')
+const chalk = require('chalk')
+const symbols = require('log-symbols')
+const inquirer = require('inquirer')
+const version = require('../package.json').version
+const templateMaps = require('../template.json')
+const spinner = ora('正在下载模板...')
+
+const questions = [
+    {
+        type: 'list',
+        name: 'template',
+        message: '请选择模板',
+        choices: [
+            {
+                name: 'react项目模板',
+                value: '1'
+            },
+            {
+                name: 'koa && ejs服务端渲染模板',
+                value: '2'
+            }
+        ]
+    }
+]
+
+program.version(version, '-v, --version')
+       .command('init <name>')
+       .action((name) => {
+            inquirer
+            .prompt(questions)
+            .then(choose => {
+                  spinner.start()
+                  download(templateMaps[choose.template], name, {clone: true}, (err) => {
+                      if(err){
+                		spinner.fail()
+                		console.log(symbols.error, chalk.red('项目创建失败'))
+                		console.log(symbols.error, chalk.red(err))
+                      }else{
+                		spinner.succeed()
+                        console.log(symbols.success, chalk.green('项目已创建\n'))
+                        console.log(`cd ./${name}\n`)
+                        if (+choose.template === 1) {
+                            console.log(' npm install\n &&\n npm start')
+                        } else if (+choose.template === 2) {
+                            console.log(' npm install\n &&\n npm run dev\n &&\n npm run server')
+                        }
+                      }
+                  })
+            })
+       })
+program.parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# 一个简易的cli工具
+
+## 安装
+```
+	npm install -g wecoder-cli
+```
+
+## 初始化项目
+```
+	wecoder init <project-name>
+```
+
+## 模板类型
+1.基于ts和webpack的一个react项目模板  
+2.基于koa和ejs的一个服务端渲染木板
