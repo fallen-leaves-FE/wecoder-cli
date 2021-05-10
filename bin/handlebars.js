40b2abefd413b266aa89957067ac999c3a5b1ce4
yangbo02 <yangbo02@longfor.com>
2021-05-10 17:59:29 +0800

    新增模板

---
+++ bin/handlebars.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const handlebars = require('handlebars')
+module.exports = (answers) => {
+  const fileName = `${answers.name}/package.json`; // 这里可以指定你需要的路径
+  const meta = {
+      name: answers.name,
+      description: answers.description,
+      author: answers.author,
+      version: answers.version
+  }                 
+  if(fs.existsSync(fileName)){
+      const content = fs.readFileSync(fileName).toString()
+      const result = handlebars.compile(content)(meta)
+      fs.writeFileSync(fileName, result)
+  }
+}
--- bin/index.js
+++ bin/index.js
@@ -7,47 +7,81 @@ const symbols = require('log-symbols')
 const inquirer = require('inquirer')
 const version = require('../package.json').version
 const templateMaps = require('../template.json')
+const handlebars = require('./handlebars')
 const spinner = ora('正在下载模板...')
 
-const questions = [
-    {
-        type: 'list',
-        name: 'template',
-        message: '请选择模板',
-        choices: [
-            {
-                name: 'react项目模板',
-                value: '1'
-            },
-            {
-                name: 'koa && ejs服务端渲染模板',
-                value: '2'
-            },
-            {
-                name: '基于webpack的typescript学习模板',
-                value: '3'
-            }
-        ]
-    }
-]
-
 program.version(version, '-v, --version')
-       .command('init <name>')
+       .command('init [name]')
        .action((name) => {
             inquirer
-            .prompt(questions)
-            .then(choose => {
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'projectName',
+                    message: '项目名称',
+                    default: name,
+                    validate (val) {
+                        if ((/[\w-]/g).test(val)) {
+                            return true
+                        }
+                        return '请输入项目名称，例如：react-ts-project'
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'version',
+                    message: '项目版本?',
+                    default: '1.0.0'
+                },
+                {
+                    type: 'input',
+                    name: 'author',
+                    message: '作者?',
+                    default: ''
+                },
+                {
+                    type: 'input',
+                    name: 'description',
+                    message: '项目描述?',
+                    default: 'This is a NodeJS project.'
+                },
+                {
+                    type: 'list',
+                    name: 'template',
+                    message: '请选择模板',
+                    choices: [
+                        {
+                            name: 'react项目模板',
+                            value: '1'
+                        },
+                        {
+                            name: 'koa && ejs服务端渲染模板',
+                            value: '2'
+                        },
+                        {
+                            name: '基于webpack的typescript学习模板',
+                            value: '3'
+                        },
+                        {
+                            name: '基于rollup的vue3.0组件库模板',
+                            value: '4'
+                        }
+                    ]
+                }
+            ])
+            .then(answers => {
                   spinner.start()
-                  download(templateMaps[choose.template], name, {clone: true}, (err) => {
+                  download(templateMaps[answers.template], answers.name, {clone: true}, (err) => {
                       if(err){
                 		spinner.fail()
                 		console.log(symbols.error, chalk.red('项目创建失败'))
                 		console.log(symbols.error, chalk.red(err))
                       }else{
+                        handlebars(answers)
                 		spinner.succeed()
                         console.log(symbols.success, chalk.green('项目已创建\n'))
                         console.log(`cd ./${name}\n`)
-                        switch (+choose.template) {
+                        switch (+answers.template) {
                             case 1:
                                 console.log(' npm install\n &&\n npm start')
                                 break
